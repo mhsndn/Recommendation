802709cd88199a98be429967d5900fea1a5f4307
mohsen <mhsn.deni@gmail.com>
2022-10-24 13:44:53 +0330

    develop

---
+++ src/components/Login/Login.js
@@ -0,0 +1,37 @@
+import classes from './Login.module.scss';
+
+const Login=()=>{
+    return(<div>
+<div className={classes.wrapper}>
+  <div className={classes.container}>
+    <div className={classes.colleft}>
+      <div className={classes.logintext}>
+        <h1>Welcome to </h1>
+        <h2>Irancell Labs</h2>
+      </div>
+      
+    </div>
+    <div className={classes.colright}>
+      <div className={classes.loginform}>
+        <h2>Login</h2>
+        <form>
+          <p>
+            <label>Username or email address<span>*</span></label>
+            <input type="text" placeholder="Username or Email" required/>
+          </p>
+          <p>
+            <label>Password<span>*</span></label>
+            <input type="password" placeholder="Password" required/>
+          </p>
+          <p>
+            <input type="submit" value="Sing In" />
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
+  </div>
+    </div>);
+}
+
+export default Login;
