6b0d78d7a6b311ac88c00a78e7c4ebb5e9053c12
mohsen <mhsn.deni@gmail.com>
2022-10-24 15:24:35 +0330

    Recommendation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
+  BrowserRouter,
   HashRouter,
 
 } from "react-router-dom";
@@ -12,11 +13,11 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       {/* <Provider store={store}> */}
           <App />
       {/* </Provider> */}
-  </HashRouter>
+  </BrowserRouter>
  </React.StrictMode>
 );
 
